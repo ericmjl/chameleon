17c783ba82064b392bb211ce88c481f23f71eff7
Eric Ma <ericmajinglong@gmail.com>
2015-11-29 10:02:28 -0500

    updated player class to work

--- game/player.py
+++ game/player.py
@@ -28,36 +28,33 @@ class Player(object):
 
             if self.has_matching_suite(pile):
                 c = sorted([c for c in self.hand if c.suite == pile.current_suite], key=lambda x:x.value)
-                print(c)
                 c = c[0]
-                pile.receive_card(c)
+                print('Dealing {0} of {1}'.format(c.value, c.suite))
+                pile.receive_card(c, new_suite=None, is_using_chameleon=False)
+                self.hand.remove(c)
+
+            elif self.has_matching_value(pile):
+                c = choice([c for c in self.hand if c.value == pile.current_value])
+                print('Dealing {0} of {1}'.format(c.value, c.suite))
+                pile.receive_card(c, new_suite=None, is_using_chameleon=False)
                 self.hand.remove(c)
 
             elif self.has_chameleon() or self.has_joker():
                 c = choice([c for c in self.hand if c.value == self.chameleon or c.value == 14])
                 new_suite = choice([c.suite for c in self.hand if c.value != self.chameleon and c.value != 14])
-                print(new_suite)
-                pile.receive_card(c, new_suite, is_using_chameleon=True)
-                self.hand.remove(c)
+                print('Dealing {0} of {1}'.format(c.value, c.suite))
+                pile.receive_card(c, new_suite=new_suite, is_using_chameleon=False)
+                self.hand.remove(c)      
 
             else:
                 # Choose a card of minimum value.
                 c = sorted([c for c in self.hand], key = lambda x:x.value)
-                print(c)
+                new_suite = None
                 c = c[0]
-                pile.receive_card(c)
+                print("No cards. Penalty with {0} of {1}.".format(c.value, c.suite))
+                self.penalty.append(c)
                 self.hand.remove(c)
 
-        # for c in self.hand:
-        #     if c.suite == pile.current_suite or c.value == pile.current_value or len(c) == 1:
-        #         pile.receive_card(c)
-        #         self.hand.remove(c)
-        #         break
-
-        #     else:
-        #         self.penalty.append(c)
-        #         self.hand.remove(c)
-        #         break        
 
     def take_card(self, deck):
         """
@@ -66,6 +63,9 @@ class Player(object):
         if deck.has_cards():
             card = deck.deal()
             self.hand.append(card)
+
+        else:
+            print('Deck has no more cards.')
 
     def penalty_score(self):
         """
@@ -98,3 +98,15 @@ class Player(object):
         """
         return bool(self.hand)
 
+    def has_matching_value(self, pile):
+        """
+        Boolean that returns whether the hand contains a card of matching value.
+        """
+
+        cv = pile.current_value
+        return cv in [c.value for c in self.hand]
+
+    def most_common_suite(self, value=None):
+        """
+        Returns the most common suite on hand.
+        """
